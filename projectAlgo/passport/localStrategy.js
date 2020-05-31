8bdc4f7547b9b91cebeab3bafa182a279092ea0a
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-05-31 16:03:08 +0900

    2020-05-31
    login log 추가 -LJH

--- projectAlgo/passport/localStrategy.js
+++ projectAlgo/passport/localStrategy.js
@@ -18,21 +18,34 @@ module.exports=(passport)=>{
   }, function (req, user_id, user_pw, done) {
         console.log("passport start.")
         connection.query(`select * from tb_user_info where user_id=${connection.escape(user_id)}`,(err,user)=>{ //유저 정보 테이블에서 조회
-            if(err)
+            if(err){
+                console.log('DB 오류!')
                 return done(null,false, {message:'DB 오류!'});
+            }    
             else if(!isNotLoggedIn(req))
+            {
+                console.log('이미 로그인 된 상태입니다!');
                 return done(null,false, {message:'이미 로그인 된 상태입니다!'});
+            }
             else if(!user[0])
+            {
+                console.log('이미 로그인 된 상태입니다!');
                 return done(null,false,{message:'존재하지 않는 아이디 입니다!'});
+            }
             //비밀번호 회원가입 시 bcrypt로 암호화
             bcrypt.compare(user_pw,user[0].user_pw,(err,result)=>{
                 if (err)
+                {
+                    console.log('bcrypt error!');
                     return done(null,false, {messsage:'bcrypt error!'});
+                }
                 else{
                     if(result){
+                        console.log('로그인 성공');
                         return done(null,{user_id:user[0].user_id,user_administratordmin:user[0].user_administrator}) //유저 아이디와 admin 여부를 저장
                     }
                     else{
+                        console.log('로그인 실패');
                         return done(null,false,{message:'비밀번호가 틀립니다!.'})
                     }
                 }
