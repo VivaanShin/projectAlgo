d09347b99fc1552f200883472f6fa09e7d651dba
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-06-03 16:42:49 +0900

    2020-06-03
    render 수정 -LJH

--- projectAlgo/routes/adminMemberGrade.js
+++ projectAlgo/routes/adminMemberGrade.js
@@ -40,7 +40,7 @@ router.get('/',async (req,res)=>{
     }
     finally{
         connection.end();
-        res.render('admin/grade_user.ejs',gradeInfoSet)
+        res.render('admin_page/grade_user.ejs',gradeInfoSet)
     }
 });
 
