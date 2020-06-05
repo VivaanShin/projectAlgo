d7e434000f20a128ad0a2f071f8ae3692850dde8
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-06-05 12:26:14 +0900

    2020-06-05
    searchPolitician 수정 -LJH

--- projectAlgo/routes/searchPolitician.js
+++ projectAlgo/routes/searchPolitician.js
@@ -17,55 +17,51 @@ const dbConfig={
 router.get('/',(req,res)=>{
     var h_area1=req.query.h_area1;
     var h_area2=req.query.h_area2;
+    var resultData={};
 
     if(typeof h_area1 !='undefined' && typeof h_area2 !='undefined'){
-        h_area1=h_area1.replace(' ','');
-        h_area2=h_area2.replace(' ','');
-        console.log('%'+h_area1+'%');
-        console.log('%'+h_area2+'%');
+        var connection=mysql.createConnection(dbConfig);
+        connection.query(`select * from tb_politician_info 
+        where sdName like ? and sggName like ?`, //선거구와 시도 이름으로 가져옴
+        ['%'+h_area1+'%','%'+h_area2+'%'],(err,politicians)=>{
+            var searchResult=[];
+            if(err)
+            {
+                console.log(err.message);
+                connection.end();
+                resultData.status=500;
+            }
+            else{
+                for(let i=0;i<politicians.length;i++){
+                    politician={
+                        politician_name:politicians[i].politician_name,
+                        jdName:politicians[i].jdName,
+                        birthday:politicians[i].birthday,
+                        career2:politicians[i].career2,
+                        img:"/images/"+politicians[i].politician_no,
+                        link:"/poltician/"+politicians[i].politician_no
+                    }
+
+                    searchResult.push(politician);
+                }                                           
+            }
+
+            resultData.searchResult=searchResult;
+        });
     }
-    var connection=mysql.createConnection(dbConfig);
-    connection.query(`select * from tb_politician_info 
-    where sdName like ? and sggName like ?` //선거구와 시도 이름으로 가져옴
-    ['%'+h_area1+'%','%'+h_area2+'%'],(err,politicians)=>{
-        var searchResult=[];
-        var status={};
-        var resultData={};
-        if(err)
-        {
-            console.log(err.message);
-            status={status:500};
-        }
-        else{
-            status={status:200};
-            for(let i=0;i<politicians.length;i++){
-                politician={
-                    politician_name:politicians[i].politician_name,
-                    jdName:politicians[i].jdName,
-                    birthday:politicians[i].birthday,
-                    career2:politicians[i].career2,
-                    img:"/images/"+politicians[i].politician_no,
-                    link:"/poltician/"+politicians[i].politician_no
-                }
 
-                searchResult.push(politician);
-            }                                           
-        }
-
-
-        resultData.searchResult=searchResult;
         
-        if(isLoggined){
-            resultData.user=req.user;
-            console.log(resultData.user);
-        }
+    if(isLoggined){
+        resultData.user=req.user;
+        console.log(resultData.user);
+    }
          //세션 상태값+모든 검색된 정치인 정보 Row
         
-        console.log(searchResult);
-        console.log(resultData);
-        connection.end();
-        res.render('search.ejs', resultData); 
-    });
+    console.log(searchResult);
+    console.log(resultData);
+    connection.end();
+    res.render('search.ejs', resultData);
 });
+   
 
 module.exports=router;
