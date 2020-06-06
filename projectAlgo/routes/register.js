b650263e161d3da09872cf0841e00c2ca157f8a3
VivaanShin <xhatmtjfla23@gmail.com>
2020-06-06 19:32:36 +0900

    update

--- projectAlgo/routes/register.js
+++ projectAlgo/routes/register.js
@@ -64,6 +64,7 @@ passport.use('local-join', new LocalStrategy({
   phoneFiled: 'user_phone',
   passReqToCallback: true
 }, function(req, user_id, user_pw, done) {
+  var joincondition = 0;
   console.log("local-join in");
   console.log(req.body);
   var user_id = req.body.user_id;
@@ -95,93 +96,71 @@ passport.use('local-join', new LocalStrategy({
           message: 'your password is mismatch'
         });
       } else {
-        console.log("등록절차")
-        // 이메일 인증 토큰값 생성
-        var newSecret = twoFactor.generateSecret();
-        var newToken = twoFactor.generateToken(newSecret.secret);
-        var user_token = newToken.token;
-        var email_url = 'ec2-3-34-124-6.ap-northeast-2.compute.amazonaws.com' + '/register_check' + '?user_email=' + user_email + '&user_token=' + user_token;
-
-        //비밀번호 해쉬값변경
-        bcrypt.hash(user_pw, null, null, function(err, hash) {
-          console.log("user_hash=" + hash);
-        });
-
-        //DB에 회원정보 저장
-        var sql2 = 'insert into tb_user_info(user_id, user_pw, user_phone, user_email, user_state, user_token) values(?,?,?,?,?,?)';
-        var params2 = [user_id, user_pw, user_phone, user_email, 0, user_token];
-        console.log("params2", params2);
-        //var query2 =
-        connection.query(sql2, params2, function(err, rows, fields) {
-          if (err) {
-            return done(null, false, {
-              message: 'DB2 error'
-            });
-          } else {
-            console.log("회원정보 입력 데이터", rows.insertId);
-            return done(null, user_id)
-          }
-
-        });
-
-        function sendMail(user_email, email_url) {
-          const mailConfig = {
-            service: 'Naver',
-            host: 'smtp.naver.com',
-            port: 587,
-            auth: {
-              user: process.env.MAIL_EMAIL,
-              pass: process.env.MAIL_PASSWORD
-            }
-          }
-          let message = {
-            from: process.env.MAIL_EMAIL,
-            to: user_email,
-            subject: '알고뽑자사이트 이메일 인증 요청 메일입니다.',
-            html: '<p><h1>해당 url 클릭시 인증이 완료됩니다. </h1> </p>' + email_url
-          }
-          let transporter = nodemailer.createTransport(mailConfig);
-          transporter.sendMail(message);
-
-
-        };
-        sendMail(user_email, email_url);
-
-        /*
-                  async sendMail(user_email) {
-                    try {
-                      const mailConfig = {
-                        service: 'Naver',
-                        host: 'smtp.naver.com',
-                        port: 587,
-                        auth: {
-                          user: process.env.MAIL_EMAIL,
-                          pass: process.env.MAIL_PASSWORD
-                        }
-                      }
-                      let message = {
-                        from: process.env.MAIL_EMAIL,
-                        to: user_email,
-                        subject: '알고뽑자사이트 이메일 인증 요청 메일입니다.',
-                        html: '<p><h1>해당 url 클릭시 인증이 완료됩니다. </h1> </p>' + email_url
-                      }
-                      let transporter = nodemailer.createTransport(mailConfig);
-                      transporter.sendMail(message);
-
-                    } catch (error) {
-                      console.log(error)
-                    }
-                  }
-        */
-        setTimeout(function() {
-          console.log("DB connection end");
-          connection.end();
-        }, 20000);
-        //connection.end();
+        console.log("회원가입 조건 만족");
+        joincondition = 1
+        return joincondition
         //res.send('<script type="text/javascript">alert("이메일을 확인하세요."); window.location="/";</script>');
       }
     }
   })
+  if(joincondition == 1){
+    // 이메일 인증 토큰값 생성
+    var newSecret = twoFactor.generateSecret();
+    var newToken = twoFactor.generateToken(newSecret.secret);
+    var user_token = newToken.token;
+    var email_url = 'ec2-3-34-124-6.ap-northeast-2.compute.amazonaws.com' + '/register_check' + '?user_email=' + user_email + '&user_token=' + user_token;
+
+    //비밀번호 해쉬값변경
+    bcrypt.hash(user_pw, null, null, function(err, hash) {
+      console.log("user_hash=" + hash);
+    });
+
+
+
+    function sendMail(user_email, email_url) {
+      const mailConfig = {
+        service: 'Naver',
+        host: 'smtp.naver.com',
+        port: 587,
+        auth: {
+          user: process.env.MAIL_EMAIL,
+          pass: process.env.MAIL_PASSWORD
+        }
+      }
+      let message = {
+        from: process.env.MAIL_EMAIL,
+        to: user_email,
+        subject: '알고뽑자사이트 이메일 인증 요청 메일입니다.',
+        html: '<p><h1>해당 url 클릭시 인증이 완료됩니다. </h1> </p>' + email_url
+      }
+      let transporter = nodemailer.createTransport(mailConfig);
+      transporter.sendMail(message);
+
+
+    };
+    sendMail(user_email, email_url);
+    //DB에 회원정보 저장
+    var sql2 = 'insert into tb_user_info(user_id, user_pw, user_phone, user_email, user_state, user_token) values(?,?,?,?,?,?)';
+    var params2 = [user_id, user_pw, user_phone, user_email, 0, user_token];
+    console.log("params2", params2);
+    //var query2 =
+    connection.query(sql2, params2, function(err, rows, fields) {
+      if (err) {
+        return done(null, false, {
+          message: 'DB2 error'
+        });
+      } else {
+        console.log("회원정보 입력 데이터", rows.insertId);
+        return done(null, user_id)
+      }
+
+    });
+  }
+
+  setTimeout(function() {
+    console.log("DB connection end");
+    connection.end();
+  }, 20000);
 }))
 
 
