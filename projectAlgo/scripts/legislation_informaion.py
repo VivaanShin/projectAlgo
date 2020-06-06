265f56d5ea42133afac58eac77fcc6dff95d4ce5
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-06-06 21:50:50 +0900

    2020-06-06
    summary 1000 자제한으로 자르기 추가 -LJH

--- projectAlgo/scripts/legislation_informaion.py
+++ projectAlgo/scripts/legislation_informaion.py
@@ -125,7 +125,7 @@ class LegislationInformaion: #OPEN API에서 의안 정보를 가져오는 클�
                                 proposeDt=legislation.find("proposedt").get_text()
                                 procDt=legislation.find("procdt").get_text() if legislation.find("procdt") else '0001-01-01'
                                 generalResult=legislation.find("generalresult").get_text() if legislation.find("generalresult") else '없음'
-                                summary=legislation.find("summary").get_text() if legislation.find("summary") else '없음'
+                                summary=legislation.find("summary").get_text()[:1000] if legislation.find("summary") else '없음'
                                 procStageCd=legislation.find("procstagecd").get_text()
                                 passGubn=legislation.find("passgubn").get_text()
                                 curr_committee=int(committee_code)
