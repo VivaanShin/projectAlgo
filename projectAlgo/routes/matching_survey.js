1ff5e108da784eafaf301e7bd943d83ce634eaad
VivaanShin <xhatmtjfla23@gmail.com>
2020-06-13 00:19:00 +0900

    matched survey update -yj

--- projectAlgo/routes/matching_survey.js
+++ projectAlgo/routes/matching_survey.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const isLoggedin=require('../scripts/confirmLogin').isLoggedIn;
 const mysql = require('mysql');
 const dbConfig={
     host     : 'localhost',
@@ -9,11 +10,15 @@ const dbConfig={
   }; //후에 DB설정에 맞게 변경
 
   router.get('/', (req, res) => {
+    if(isLoggedin(req)){ //로그인 정보
+            resultData.user=req.user;
+        }
     console.log('survey join');
     res.render('matching_survey');
   });
 
- 
+
+
 
 
 module.exports = router;
