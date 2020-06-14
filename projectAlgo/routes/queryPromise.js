6aede07f1043ee4cd99e69753cdd5a578480c4df
VivaanShin <xhatmtjfla23@gmail.com>
2020-06-15 04:23:23 +0900

    update

--- projectAlgo/routes/queryPromise.js
+++ projectAlgo/routes/queryPromise.js
@@ -289,7 +289,7 @@ exports.getUserGrade=function getUserGrade(connection){ //connection 하나를 �
 
 exports.getBlackUserGrade=function getBlackUserGrade(user_id, connection){ //connection 하나를 전달 받아서 사용,동기형으로 tn_user_politician_grade를 모두 가져옴
     return new Promise((resolve,reject)=>{
-        connection.query(`select * from tb_gradeinfo_record as tr, tb_politician_info as pi where tr.politician_no=pi.politician_no and tr.user_id = ?`,[user_id],
+        connection.query(`select * from tb_gradeinfo_record as tr, tb_politician_info as pi where tr.politician_no=pi.politician_no and tr.user_id = ?`,[user_id]
         ,(err,userGrade)=>{
             if(err)
                 reject(err);
