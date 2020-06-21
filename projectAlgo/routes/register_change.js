a476c1a5c0b0c2d3a9427ed742bd7fd623285cf6
VivaanShin <xhatmtjfla23@gmail.com>
2020-06-21 16:34:43 +0900

    update

--- projectAlgo/routes/register_change.js
+++ projectAlgo/routes/register_change.js
@@ -42,8 +42,57 @@ router.post('/', (req, res) => {
     resultData.user = req.user;
     resultData.user.user_interest_check = req.session.user_interest_check;
   }
+  var user_id = req.user.user_id;
+  var user_pw = req.body.user_pw;
+  var user_pw_check = req.body.user_pw_check;
+  var user_email = req.body.user_email;
+  var user_phone = req.body.user_phone;
 
-  res.render('register_change', resultData);
+  var connection = mysql.createConnection(dbConfig);
+  connection.connect();
+
+  function userinfo_change(){
+    return new Promise(function(resolve, reject){
+      if(user_pw == user_pw_check){
+        bcrypt.hash(user_pw, null, null, function(err, hash){
+          hash_pw = hash
+        })
+        resolve(hash_pw)
+      }
+      else {
+        console.log(error)
+      }
+    })
+  }.then(function(hash_pw){
+    return new Promise(function(resolve, reject){
+      var sql = 'update tb_user_info set user_pw = ? , user_email = ? , user_phone = ? where user_id = ?';
+      var params = [hash_pw, user_email, user_phone, user_id];
+      connection.query(sql, params, function(err, results, fields) {
+        if(err){
+          console.oog(err);
+        }else{
+          console.log("success");
+          resolve();
+          res.render('home', resultData);
+          connection.end();
+        }
+
+
+      })
+    })
+  }).catch(function(err){
+    console.log('error',err);
+  })
+
+
+
+
+
+
+
+
+
+
 
 });
 
--- projectAlgo/routes/withdrawal.js
+++ projectAlgo/routes/withdrawal.js
@@ -11,7 +11,43 @@ const dbConfig = {
   database: 'project_algo'
 };
 
+router.get('/', (req, res) => {
+  var resultData = {};
+  /*
+  if (isLoggedin(req)) { //로그인 정보
+    resultData.user = req.user;
+  }
+  */
 
+  if (isLoggedin(req)) {
+    resultData.user = req.user;
+    resultData.user.user_interest_check = req.session.user_interest_check;
+  }
+  var user_id = req.body.user_id;
+  var connection = mysql.createConnection(dbConfig);
+  connection.connect();
+
+  var sql = 'delete from tb_user_info where user_id = ?';
+  connection.query(sql2, user_id, function(err, rows, fields) {
+    console.log("query in");
+    if (err) {
+      console.log("query err");
+      return done(null, false, {
+        message: 'DB2 error'
+      });
+    } else {
+      return done(null, true, {
+        message: 'success'
+      });
+    }
+  });
+
+
+
+
+  res.render('home', resultData);
+  connection.end();
+});
 
 
 
