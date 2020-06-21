f427de5b9c25a29dffe085fb968d46a19f7669ad
VivaanShin <xhatmtjfla23@gmail.com>
2020-06-21 15:35:15 +0900

    update

--- projectAlgo/routes/withdrawal.js
+++ projectAlgo/routes/withdrawal.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql');
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
+var session = require('express-session');
+const isLoggedin = require('../scripts/confirmLogin').isLoggedIn;
 const router = express.Router();
 const dbConfig = {
   host: 'localhost',
