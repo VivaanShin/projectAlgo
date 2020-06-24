afde9f2f0192b0b74c69845ed90c59955244a8e0
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-06-24 22:19:42 +0900

    2020-06-24
    평점 재수정 - LJH

--- projectAlgo/routes/politicianUser.js
+++ projectAlgo/routes/politicianUser.js
@@ -87,7 +87,7 @@ router.get('/:politician_no',async (req,res)=>{ //기본 신상 정보 라우터
         resultData.gradeScore=gradeScore;
         var gradeList=[]; //정치인 주당 평균
         var weekNewsInfo=[]; //주당 뉴스
-        var weekDay=moment().isoWeekday(1).format('YYYY-MM-DD'); //해당 주 일요일부터 4주까지
+        var weekDay=moment().isoWeekday(0).format('YYYY-MM-DD'); //해당 주 일요일부터 4주까지
         
         
         for (let i=0;i<4;i++){//4주 까지 가져옴
@@ -188,7 +188,7 @@ router.put('/:politician_no/grade',async (req,res)=>{ //정치인 평점 등록
     var grade_score=Number(req.body.grade);
     var user_id=req.user.user_id;
     var politician_no=req.params.politician_no;
-    var dayInfo=moment().isoWeekday(1).format('YYYY-MM-DD');
+    var dayInfo=moment().isoWeekday(0).format('YYYY-MM-DD');
     var sendAlert="";
     console.log(grade_score);
 
